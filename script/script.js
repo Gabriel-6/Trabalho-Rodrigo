2ad9ca75afa3716c334a177c0604c7c7eab94efb
Gabriel-Inoveh <116814159+Gabriel-Inoveh@users.noreply.github.com>
2022-11-04 15:34:00 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -42,7 +42,7 @@ function on_input() {
     var jogo3su = parseInt(document.querySelector('.sui3').value)
 
     //Jogo Suiça x Camarões
-    if ((jogo1su == jogo1c) && jogo1su != '' && jogo1c != '') {
+    if ((jogo1su == jogo1c)) {
         ptsu+=1
         ptc+=1
         golsu+= jogo1su
@@ -55,7 +55,7 @@ function on_input() {
         change_camaroes(ptc, jc, vitc, saldoc, golc)
         sortTable()
 
-    } else if((jogo1su > jogo1c) && jogo1su != '' && jogo1c != '') {
+    } else if((jogo1su > jogo1c)) {
         ptsu+=3
         golsu+=jogo1su
         golc+=jogo1c
@@ -67,7 +67,8 @@ function on_input() {
         change_camaroes(ptc, jc, vitc, saldoc, golc)
         change_suica(ptsu, jsu, vitsu, saldosu, golsu)
         sortTable()
-    } else if((jogo1su < jogo1c) && jogo1su != '' && jogo1c != '') {
+        
+    } else if((jogo1su < jogo1c)) {
         ptc+=3
         golsu+=jogo1su
         golc+=jogo1c
@@ -79,11 +80,11 @@ function on_input() {
         change_camaroes(ptc, jc, vitc, saldoc, golc)
         change_suica(ptsu, jsu, vitsu, saldosu, golsu)
         sortTable()
+        console.log(jogo1c)
     }
 
-
     //Jogo Brasil x Servia
-    if ((jogo1b == jogo1se) && jogo1b != '' && jogo1se != '') {
+    if ((jogo1b == jogo1se)) {
         ptb+=1
         ptse+=1
         golb+= jogo1b
@@ -95,7 +96,7 @@ function on_input() {
         change_brasil(ptb, jb, vitb, saldob, golb)
         change_servia(ptse, jse, vitse, saldose, golse)
         sortTable()
-    } else if((jogo1b > jogo1se) && jogo1b != '' && jogo1se != '') {
+    } else if((jogo1b > jogo1se)) {
         ptb+=3
         golb+=jogo1b
         golse+=jogo1se
@@ -107,7 +108,7 @@ function on_input() {
         change_brasil(ptb, jb, vitb, saldob, golb)
         change_servia(ptse, jse, vitse, saldose, golse)
         sortTable()
-    } else if((jogo1b < jogo1se) && jogo1b != '' && jogo1se != '') {
+    } else if((jogo1b < jogo1se)) {
         ptse+=3
         golb+=jogo1b
         golse+=jogo1se
@@ -121,9 +122,8 @@ function on_input() {
         sortTable()
     }
 
-
     //Jogo Camarões x Servia
-    if ((jogo2c == jogo2se) && jogo2c != '' && jogo2se != '') {
+    if ((jogo2c == jogo2se)) {
         ptc+=1
         ptse+=1
         golc+= jogo2c
@@ -135,7 +135,7 @@ function on_input() {
         change_servia(ptse, jse, vitse, saldose, golse)
         change_camaroes(ptc, jc, vitc, saldoc, golc)
         sortTable()
-    } else if((jogo2c > jogo2se) && jogo2c != '' && jogo2se != '') {
+    } else if((jogo2c > jogo2se)) {
         ptc+=3
         golc+=jogo2c
         golse+=jogo2se
@@ -147,7 +147,7 @@ function on_input() {
         change_servia(ptse, jse, vitse, saldose, golse)
         change_camaroes(ptc, jc, vitc, saldoc, golc)
         sortTable()
-    } else if((jogo2c < jogo2se) && jogo2c != '' && jogo2se != '') {
+    } else if((jogo2c < jogo2se)) {
         ptse+=3
         golc+=jogo2c
         golse+=jogo2se
@@ -161,9 +161,8 @@ function on_input() {
         sortTable()
     }
 
-
     //Jogo Brasil x Suica
-    if ((jogo2b == jogo2su) && jogo2b != '' && jogo2su != '') {
+    if ((jogo2b == jogo2su)) {
         ptb+=1
         ptsu+=1
         golb+= jogo2b
@@ -175,7 +174,7 @@ function on_input() {
         change_brasil(ptb, jb, vitb, saldob, golb)
         change_suica(ptsu, jsu, vitsu, saldosu, golsu)
         sortTable()
-    } else if((jogo2b > jogo2su) && jogo2b != '' && jogo2su != '') {
+    } else if((jogo2b > jogo2su)) {
         ptb+=3
         golb+=jogo2b
         golsu+=jogo2su
@@ -187,7 +186,7 @@ function on_input() {
         change_brasil(ptb, jb, vitb, saldob, golb)
         change_suica(ptsu, jsu, vitsu, saldosu, golsu)
         sortTable()
-    } else if((jogo2b < jogo2su) && jogo2b != '' && jogo2su != '') {
+    } else if((jogo2b < jogo2su)) {
         ptsu+=3
         golb+=jogo2b
         golsu+=jogo2su
@@ -201,9 +200,8 @@ function on_input() {
         sortTable()
     }
 
-
     //Jogo Camarões x Brasil
-    if ((jogo3c == jogo3b) && jogo3c != '' && jogo3b != '') {
+    if ((jogo3c == jogo3b)) {
         ptc+=1
         ptb+=1
         golc+= jogo3c
@@ -216,7 +214,7 @@ function on_input() {
         change_brasil(ptb, jb, vitb, saldob, golb)
         sortTable()
 
-    } else if((jogo3c > jogo3b) && jogo3c != '' && jogo3b != '') {
+    } else if((jogo3c > jogo3b)) {
         ptc+=3
         golc+=jogo3c
         golb+=jogo3b
@@ -228,7 +226,7 @@ function on_input() {
         change_camaroes(ptc, jc, vitc, saldoc, golc)
         change_brasil(ptb, jb, vitb, saldob, golb)
         sortTable()
-    } else if((jogo3c < jogo3b) && jogo3c != '' && jogo3b != '') {
+    } else if((jogo3c < jogo3b)) {
         ptb+=3
         golc+=jogo3c
         golb+=jogo3b
@@ -242,9 +240,8 @@ function on_input() {
         sortTable()
     }
 
-
     //Jogo Servia x Suiça
-    if ((jogo3se == jogo3su) && jogo3se != '' && jogo3su != '') {
+    if ((jogo3se == jogo3su)) {
         ptse+=1
         ptsu+=1
         golse+= jogo3se
@@ -256,7 +253,7 @@ function on_input() {
         change_servia(ptse, jse, vitse, saldose, golse)
         change_suica(ptsu, jsu, vitsu, saldosu ,golsu)
         sortTable()
-    } else if((jogo3se > jogo3su) && jogo3se != '' && jogo3su != '') {
+    } else if((jogo3se > jogo3su)) {
         ptse+=3
         golse+=jogo3se
         golsu+=jogo3su
@@ -268,7 +265,7 @@ function on_input() {
         change_servia(ptse, jse, vitse, saldose, golse)
         change_suica(ptsu, jsu, vitsu, saldosu ,golsu)
         sortTable()
-    } else if((jogo3se < jogo3su) && jogo3se != '' && jogo3su != '') {
+    } else if((jogo3se < jogo3su)) {
         ptsu+=3
         golse+=jogo3se
         golsu+=jogo3su
@@ -282,13 +279,6 @@ function on_input() {
         sortTable()
     }
 
-}
-
-function better_play(ptb, ptsu, ptc, ptse){
-    var pontuacao = [ptb, ptsu, ptc, ptse]
-    var lista = pontuacao.sort()
-    
-    return lista
 }
 
 function add_brasil(ptb, jb, vitb, saldob, golb) {  
